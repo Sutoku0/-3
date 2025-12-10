82b800066af71cf3c814eb9183e17ec47ed120a6
Sutoku0 <gooolubuk@mail.ru>
2025-12-10 20:53:46 +0300

    Task 3

---
+++ task 3.py
@@ -0,0 +1,8 @@
+USD_TO_RUB = 95.50
+
+def convert_usd_to_rub(amount_usd):
+    return amount_usd * USD_TO_RUB
+
+amount = float(input("Введите сумму в долларах: "))
+
+print(f"{amount} USD = {convert_usd_to_rub(amount):.2f} RUB")
